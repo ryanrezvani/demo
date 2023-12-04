02de07acafcaf43d16e208bafdaa39dd0f960ec7
Ryan Rezvani <ryanrezvani@google.com>
2023-12-03 20:15:11 -0500

    adding even more packages from opentelemetry

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,8 @@ import com.google.cloud.logging.Severity;
 import java.util.Collections;
 import io.grpc.LoadBalancerRegistry;
 import io.grpc.internal.PickFirstLoadBalancerProvider;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 
 /**
  * Hello world!
@@ -19,6 +21,7 @@ public class App {
     String logName = "my-java-log"; // "my-log";
     String textPayload = "mysearchkeywordjava";
     LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
+
     // Instantiates a client
     try (Logging logging = LoggingOptions.getDefaultInstance().getService()) {
 
@@ -37,4 +40,9 @@ public class App {
     }
     System.out.printf("Logged: %s%n", textPayload);
     }
+    
+    public OpenTelemetry openTelemetry() {
+      return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+    }
+
 }
